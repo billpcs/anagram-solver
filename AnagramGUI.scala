e1aa44c93d820e5edcefdf74ef88fed90a51922f
Bill Panagiotopoulos <panagiotopoulosbill@yahoo.com>
2014-08-23 00:51:55 +0300

    Create AnagramGUI.scala

---
+++ AnagramGUI.scala
@@ -0,0 +1,66 @@
+import AnagSolverInScala._
+import scala.swing._
+import scala.swing.BorderPanel.Position._
+import event._
+import java.awt.{ Color, Graphics2D }
+import java.awt.datatransfer._
+import java.io._
+import scala.io.Source
+import scala.language.postfixOps
+
+
+
+object AnagramGUI extends SimpleSwingApplication {
+
+	override def main(args: Array[String]) = super.main(args)
+
+	def top = new MainFrame {
+
+		val entryPoint = new TextArea(4,20){
+			text = "Enter your word here!"
+			font = new Font("Monospace", java.awt.Font.ITALIC , 20)
+			background = Color.white
+			foreground = Color.black
+		}
+	
+		val outPoint = new TextArea(4,20){
+			font = new Font("Monospace", java.awt.Font.BOLD , 20)
+			background = Color.black
+			foreground = Color.white
+			editable = false
+		}
+
+		val ipaButton = new Button{
+			text = "Solve it for me!"
+		}
+
+		val scrollbar = new ScrollPane() {
+      		visible = true
+    	} 
+
+
+    	menuBar = new MenuBar {
+    		contents += new Menu("File") {
+    			contents += new MenuItem(Action("Quit") {sys.exit(0)} )
+    		}
+    	}
+
+    	listenTo(ipaButton)
+    	reactions += {
+    		case ButtonClicked(component) if(component==ipaButton)=>
+            outPoint.text = ""
+    		outPoint.foreground = Color.white
+    		solveIt("americanenglishCAPS" , entryPoint.text.toUpperCase ) foreach(p=> outPoint.text += p.toLowerCase+"\n")
+    	}
+
+    	contents = new BorderPanel {       
+        	add(new ScrollPane(entryPoint), BorderPanel.Position.West)
+        	add(new ScrollPane(outPoint) , BorderPanel.Position.East)
+        	layout(ipaButton) = South
+      	}
+
+
+	}	
+
+	
+}
